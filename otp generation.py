edbab68c109b45d21366aa91029c1ca28753c060
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-04-29 20:37:20 +0530

    Update otp generation.py

--- otp generation.py
+++ otp generation.py
@@ -1,6 +1,6 @@
 import random
-print("HELLO WE ARE HERE TO ASSIST YOU TO YOUR GENERATE OTP")
-w=int(input("ENTER THE LENGTH OF THE OTP YOU WANT:"))
+print("HELLO WE ARE HERE TO ASSIST YOU TO GENERATE YOUR OTP")
+w=int(input("PLEASE ENTER THE LENGTH OF THE OTP YOU WANT:"))
 u=random.random()
 o=str(u)[-w:]
 print(o)

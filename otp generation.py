97b2374c719316e35ed908621aad697184fc96a4
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-04-30 14:13:57 +0530

    Update otp generation.py

--- otp generation.py
+++ otp generation.py
@@ -1,3 +1,8 @@
+'''One-time Passwords (OTP) is a password that is valid for only one login session or
+transaction in a computer or a digital device. Now a days OTP’s are used in almost every service like
+Internet Banking, online transactions, etc. They are generally combination of 4 or 6 numeric digits or
+a 6-digit alphanumeric.'''
+
 import random
 print("HELLO WE ARE HERE TO ASSIST YOU TO GENERATE YOUR OTP")
 w=int(input("PLEASE ENTER THE LENGTH OF THE OTP YOU WANT:"))
